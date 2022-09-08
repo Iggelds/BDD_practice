c4d0c376f4f2ee86cd6acf1e8100dc6a973c2c40
Aleksandr Guido <iggelds@gmail,com>
2022-09-08 12:02:59 +0200

    implement GamingChairsPage

--- src/main/java/org/example/pageobject/pages/GamingChairsPage.java
+++ src/main/java/org/example/pageobject/pages/GamingChairsPage.java
@@ -2,6 +2,8 @@ package org.example.pageobject.pages;
 
 import org.example.pageobject.BasePage;
 import org.example.pageobject.modules.ProfileDropDownModule;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,6 +11,27 @@ import org.openqa.selenium.support.FindBy;
 public class GamingChairsPage extends BasePage {
     @FindBy(xpath = "//summary[@class=\"Header-link\"]/img")
     private WebElement profileDropDownButton;
+
+    @FindBy(xpath = "(//*[@id=\"brandsRefinements\"]//li)[2]")
+    private WebElement brandLi;
+
+    @FindBy(xpath = "//*[@id=\"low-price\"]")
+    private WebElement low_price;
+
+    @FindBy(xpath = "//*[@id=\"high-price\"]")
+    private WebElement high_price;
+
+    @FindBy(xpath = "//*[@id=\"priceRefinements\"]//form")
+    private WebElement submitForm;
+
+    @FindBy(xpath = "//*[@class=\"a-dropdown-prompt\"]")
+    private WebElement dropDownPrompt;
+
+    @FindBy(xpath = "//*[@id=\"s-result-sort-select_1\"]")
+    private WebElement sort_select;
+
+    private WebElement brandInput = brandLi.findElement(By.tagName("input"));
+
 
     public GamingChairsPage(WebDriver webDriver) {
         super(webDriver);
@@ -18,5 +41,25 @@ public class GamingChairsPage extends BasePage {
         profileDropDownButton.click();
 
         return new ProfileDropDownModule(webDriver);
+    }
+
+    public void filterByBrand() {
+        brandInput.sendKeys(Keys.SPACE);
+    }
+
+    public String findBrand() {
+        String label = brandLi.getAttribute("aria-label");
+        return label != null ? label.toLowerCase() : "";
+    }
+
+    public void filterByPrice(double low, double high) {
+        low_price.sendKeys(String.valueOf(low));
+        high_price.sendKeys(String.valueOf(high));
+        submitForm.submit();
+    }
+
+    public void sortingByLowToHighPrice() {
+        dropDownPrompt.click();
+        sort_select.click();
     }
 }
